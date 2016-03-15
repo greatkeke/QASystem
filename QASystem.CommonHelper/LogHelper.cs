f5fa26e8ea9a2445673b0c0086a2975d5c2e7348
kexxxfeng <fengxxxke@gmail.com>
2016-03-16 07:20:23 +0800

    异常处理、错误页面。

---
+++ QASystem.CommonHelper/LogHelper.cs
@@ -0,0 +1,69 @@
+﻿using log4net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace QASystem.CommonHelper
+{
+    /// <summary>
+    /// 错误消息的（内存）队列。当出现异常之后，
+    /// 直接把错误消息放到内存的队列里去。
+    /// 然后程序继续往下执行，给用户反馈信息。不会阻塞用户的响应。
+    /// </summary>
+    public class LogHelper
+    {
+        /// <summary>
+        /// 日志
+        /// </summary>
+        private static ILog _log = LogManager.GetLogger("QA.Log");
+
+        /// <summary>
+        /// 异常信息的队列
+        /// </summary>
+        private static readonly Queue<string> ExcMsg;
+
+        static LogHelper()
+        {
+            ExcMsg = new Queue<string>();
+            ThreadPool.QueueUserWorkItem(u =>
+            {
+                while (true)
+                {
+                    string str = string.Empty;
+
+                    if (ExcMsg == null)
+                    {
+                        continue;
+                    }
+
+                    lock (ExcMsg)
+                    {
+                        if (ExcMsg.Count > 0)
+                            str = ExcMsg.Dequeue();
+                    }
+                    //往日志文件里面写就可以了。
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        _log.Error(str);
+                    }
+                    if (!ExcMsg.Any())
+                    {
+                        Thread.Sleep(30);
+                    }
+                }
+            });
+        }
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="logMsg">日志内容</param>
+        public static void WriteLog(string logMsg)
+        {
+            lock (ExcMsg)
+            {
+                ExcMsg.Enqueue(logMsg);
+            }
+        }
+    }
+}
--- QASystem.Web/Global.asax.cs
+++ QASystem.Web/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using MySql.Data.Entity;
+using QASystem.CommonHelper;
 using QASystem.Web.Infrastructure;
+using System;
 using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -17,6 +19,11 @@ namespace QASystem.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            LogHelper.WriteLog(exception.ToString());
         }
     }
 }
