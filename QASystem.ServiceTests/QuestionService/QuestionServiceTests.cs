f1c3a0400eed00a35b7c75307c9d4036d0861949
kexxxfeng <fengxxxke@gmail.com>
2016-03-16 15:55:01 +0800

    发布到服务器版本

---
+++ QASystem.ServiceTests/QuestionService/QuestionServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QASystem.ServiceTests;
+
+namespace QASystem.Service.QuestionService.Tests
+{
+    [TestClass()]
+    public class QuestionServiceTests
+    {
+        [TestMethod()]
+        public void ListBySubjectTest()
+        {
+            var res = CommonHelper.QuestionService.ListBySubject(0, 0, 15);
+            Assert.AreEqual(true, res.Count > 0);
+        }
+    }
+}
---
+++ QASystem.ServiceTests/SubjectService/SubjectServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QASystem.ServiceTests;
+using System.Linq;
+
+namespace QASystem.Service.SubjectService.Tests
+{
+    [TestClass()]
+    public class SubjectServiceTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var res = CommonHelper.SubjectService.GetAll();
+            Assert.AreEqual(true, res.Count() > 0);
+        }
+    }
+}
